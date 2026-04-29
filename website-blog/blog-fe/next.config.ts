68e892ddd15dd803863d61d6b312654f7d513f16
VUDUCQUY <quydky2k5@gmail.com>
2026-04-30 02:17:56 +0700

    fix(fe): fix modal layout and turbopack root

--- website-blog/blog-fe/next.config.ts
+++ website-blog/blog-fe/next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const nextConfig: NextConfig = {
   /* config options here */
   turbopack: {
-    root: path.resolve(__dirname, "../../"),
+    root: path.resolve(process.cwd(), "../../"),
   },
   async rewrites() {
     return [
